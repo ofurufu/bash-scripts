4c350c6e9b30215f5ba4ee4580b5cc271cf4be44
ofurufu <ayodeleadewuyia+github@gmail.com>
2026-02-06 22:00:11 +0100

    Applied loop to fetch error patterns.

--- loganalyzer/logs/log_analyzer.sh
+++ loganalyzer/logs/log_analyzer.sh
@@ -10,8 +10,6 @@
 
 # Variable Definition
 LOG_DIR="/mnt/c/Users/ME/bash-scripts/loganalyzer/logs"
-APP_LOG_FILE="application.log"
-SYS_LOG_FILE="system.log"
 
 # ERROR_PATTERNS
 ERROR_PATTERNS=("ERROR" "FATAL" "CRITICAL")
@@ -21,22 +19,21 @@ echo "=========================="
 
 echo -e "\nList of log files updated in the last 24 hours."
 LOG_FILES=$(find  $LOG_DIR -name "*.log" -mtime -1)
-echo $LOG_FILES
+echo "$LOG_FILES"
 
-echo -e "\nSearching ERROR logs in application.log file."
-grep -c "${ERROR_PATTERNS[0]}" "$LOG_DIR/$APP_LOG_FILE"
+for LOG_FILE in $LOG_FILES; do
+    echo -e "\nSearching ${ERROR_PATTERNS[0]} logs in $LOG_FILE file."
+    grep    "${ERROR_PATTERNS[0]}" "$LOG_FILE"
 
-echo -e "\nNumber of ERROR logs found in application.log"
-grep -c "${ERROR_PATTERNS[0]}" "$LOG_DIR/$APP_LOG_FILE"
+    echo -e "\nNumber of ${ERROR_PATTERNS[0]} logs found in $LOG_FILE"
+    grep -c "${ERROR_PATTERNS[0]}" "$LOG_FILE"
 
-echo -e "\nNumber of FATAL logs found in application.log"
-grep -c "${ERROR_PATTERNS[1]}" "$LOG_DIR/$APP_LOG_FILE"
+    echo -e "\nSearching ${ERROR_PATTERNS[1]} logs in $LOG_FILE file."
+    grep    "${ERROR_PATTERNS[1]}" "$LOG_FILE"
+    
+    echo -e "\nNumber of ${ERROR_PATTERNS[1]} logs found in $LOG_FILE"
+    grep -c "${ERROR_PATTERNS[1]}" "$LOG_FILE"
 
-echo -e "\nNumber of FATAL logs found in application.log"
-grep -c "${ERROR_PATTERNS[1]}" "$LOG_DIR/$APP_LOG_FILE"
-
-echo -e "\nNumber of CRIRITAL logs found in $SYS_LOG_FILE."
-grep -c "${ERROR_PATTERNS[2]}" "$LOG_DIR/$SYS_LOG_FILE"
-
-echo -e "\nCRITICAL logs in the $SYS_LOG_FILE file"
-grep -c "${ERROR_PATTERNS[2]}" "$LOG_DIR/$SYS_LOG_FILE"
+    echo -e "\nNumber of ${ERROR_PATTERNS[2]} logs found in $LOG_FILE"
+    grep -c "${ERROR_PATTERNS[2]}" "$LOG_FILE"
+done

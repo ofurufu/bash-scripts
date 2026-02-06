22124e9e18346204853fec8d72e08db6e0011b37
ofurufu <ayodeleadewuyia+github@gmail.com>
2026-02-06 18:33:15 +0100

    Eliminating all hard-coded variables.

--- loganalyzer/logs/log_analyzer.sh
+++ loganalyzer/logs/log_analyzer.sh
@@ -13,22 +13,29 @@ LOG_DIR="/mnt/c/Users/ME/bash-scripts/loganalyzer/logs"
 APP_LOG_FILE="application.log"
 SYS_LOG_FILE="system.log"
 
+# ERROR_PATTERNS
+ERROR_PATTERNS=("ERROR" "FATAL" "CRITICAL")
+
 echo "###   Analysing logs   ###"
 echo "=========================="
 
 echo -e "\nList of log files updated in the last 24 hours."
 find  $LOG_DIR -name "*.log" -mtime -1
 
-echo -e "\nSearching ERROR logs in application logs."
-grep "ERROR" "$LOG_DIR/$APP_LOG_FILE"
+echo -e "\nSearching ERROR logs in application.log file."
+grep -c "${ERROR_PATTERNS[0]}" "$LOG_DIR/$APP_LOG_FILE"
 
-echo -e "\nNumber of errors found in application logs."
-grep -c "ERROR" "$LOG_DIR/$APP_LOG_FILE
-echo -e "\nThe last error in application logs."
-grep -c "FATAL" "$LOG_DIR/$APP_LOG_FILE" | tail -1
+echo -e "\nNumber of ERROR logs found in application.log"
+grep -c "${ERROR_PATTERNS[0]}" "$LOG_DIR/$APP_LOG_FILE"
 
-echo -e "\nThe FATAL errors from system logs."
-grep "FATAL" "$LOG_DIR/$SYS_LOG_FILE"
+echo -e "\nNumber of FATAL logs found in application.log"
+grep -c "${ERROR_PATTERNS[1]}" "$LOG_DIR/$APP_LOG_FILE"
 
-echo -e "\nNumber of FATAL errors in the system log."
-grep -c "FATAL" "$LOG_DIR/$SYS_LOG_FILE"
+echo -e "\nNumber of FATAL logs found in application.log"
+grep -c "${ERROR_PATTERNS[1]}" "$LOG_DIR/$APP_LOG_FILE"
+
+echo -e "\nNumber of CRIRITAL logs found in $SYS_LOG_FILE."
+grep -c "${ERROR_PATTERNS[2]}" "$LOG_DIR/$SYS_LOG_FILE"
+
+echo -e "\nCRITICAL logs in the $SYS_LOG_FILE file"
+grep -c "${ERROR_PATTERNS[2]}" "$LOG_DIR/$SYS_LOG_FILE"

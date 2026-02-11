0c748e0151f8ed495be135bcfc3d3ef3af2a28d2
ofurufu <ayodeleadewuyia+github@gmail.com>
2026-02-11 23:43:34 +0100

    Created Report File to disallow console output.

--- loganalyzer/logs/log_analyzer.sh
+++ loganalyzer/logs/log_analyzer.sh
@@ -13,26 +13,29 @@ LOG_DIR="/mnt/c/Users/ME/bash-scripts/loganalyzer/logs"
 
 # ERROR_PATTERNS
 ERROR_PATTERNS=("ERROR" "FATAL" "CRITICAL")
+REPORT_FILE="/mnt/c/Users/ME/bash-scripts/loganalyzer/logs/log_analyzer_report.txt"
 
-echo "###   Analysing logs   ###"
-echo "=========================="
+echo "###   Analysing logs   ###" > "$REPORT_FILE"
+echo "==========================" >> "$REPORT_FILE"
 
-echo -e "\nList of log files updated in the last 24 hours."
-LOG_FILES=$(find  $LOG_DIR -name "*.log" -mtime -1)
-echo "$LOG_FILES"
+echo -e "\nList of log files updated in the last 24 hours." >> "$REPORT_FILE"
+LOG_FILES=$(find  $LOG_DIR -name "*.log" -mtime -1) 
+echo "$LOG_FILES" >> "$REPORT_FILE"
 
 for LOG_FILE in $LOG_FILES; do
-    echo -e "\n"
-    echo "================================================"
-    echo "=============$LOG_FILES========================="
-    echo "================================================"
+    echo -e "\n" >> "$REPORT_FILE"
+    echo "================================================" >> "$REPORT_FILE"
+    echo "=============$LOG_FILES=========================" >> "$REPORT_FILE"
+    echo "================================================" >> "$REPORT_FILE"
 
     for PATTERN in ${ERROR_PATTERNS[@]}; do
 
-        echo -e "\nSearching $PATTERN logs in $LOG_FILE file."
-        grep    "$PATTERN" "$LOG_FILE"
+        echo -e "\nSearching $PATTERN logs in $LOG_FILE file." >> "$REPORT_FILE"
+        grep    "$PATTERN" "$LOG_FILE" >> "$REPORT_FILE"
 
-        echo -e "\nNumber of $ERROR_PATTERN logs found in $LOG_FILE"
-        grep -c "$PATTERN" "$LOG_FILE"
+        echo -e "\nNumber of $ERROR_PATTERN logs found in $LOG_FILE" >> "$REPORT_FILE"
+        grep -c "$PATTERN" "$LOG_FILE" >> "$REPORT_FILE"
     done
 done
+
+echo -e "\nLog Analysis completed and report saved in: $REPORT_FILE"
